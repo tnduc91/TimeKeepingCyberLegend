d524c32eebe892d74be7fc6aac6b4832b81935ae
tnduc91@gmail.com <tnduc91@gmail.com>
2020-12-21 21:20:11 +0700

    Entity Framework mapping

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TimeKeepingYaz.Models;
 
 namespace TimeKeepingYaz.Controllers
 {
     public class EmployeeController : Controller
     {
+        private TimeKeepingContext _timeKeepingContext = new TimeKeepingContext();
         // GET: Employee
         public ActionResult Index()
         {
+            var emp = _timeKeepingContext.Statuses.ToList();
             return View();
         }
     }
---
+++ Models/TimeKeepingContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace TimeKeepingYaz.Models
+{
+    public class TimeKeepingContext : DbContext
+    {
+        public TimeKeepingContext() : base("TimeKeepingConnectionString")
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<DailyCheckin> DailyCheckins { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
